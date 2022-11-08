5cc38e9dd783249a92bd4c4e52437be1807053c7
Emerson <emerapmelo@gmail.com>
2022-11-08 16:09:50 -0300

    feat: Requisito 1 concluído
    Implementada a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,22 @@
 # Requisito 1
+import time
+import requests
+
+
 def fetch(url):
-    pass
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"},
+            timeout=3
+        )
+        if response.status_code == 200:
+            return response.text
+        elif response.status_code:
+            return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
