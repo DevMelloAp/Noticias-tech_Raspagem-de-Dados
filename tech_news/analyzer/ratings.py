6f0dcb0f0972a03ae1e0b1a79ed40dad365cd902
Emerson <emerapmelo@gmail.com>
2022-11-12 18:48:17 -0300

    feat: Requisito 10 concluído
    Implementada a função top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
 # Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    list_top_5_news = []
+
+    news = find_news()
+    news_sorted_by_title = sorted(
+        news, key=lambda row: row['title'], reverse=0
+    )
+    news_sorted_by_comments_count = sorted(
+        news_sorted_by_title, key=lambda row: row['comments_count'], reverse=1
+    )
+
+    for new in news_sorted_by_comments_count:
+        list_top_5_news.append((new["title"], new["url"]))
+
+    return list_top_5_news[0:5]
 
 
 # Requisito 11
