fecb6c47be836933b71cb2c52020ed607f0d0ca2
Emerson <emerapmelo@gmail.com>
2022-11-14 16:28:41 -0300

    feat: Requisito 13 concluído
    Implementadas as funcionalidades do menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,5 +1,23 @@
 # Requisito 12
 import sys
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
+from tech_news.analyzer.search_engine import (
+    search_by_category, search_by_date, search_by_tag, search_by_title)
+
+from tech_news.scraper import get_tech_news
+
+
+def function(option, value):
+    if option == "0":
+        return get_tech_news(int(value))
+    elif option == "1":
+        return search_by_title(value)
+    elif option == "2":
+        return search_by_date(value)
+    elif option == "3":
+        return search_by_tag(value)
+    else:
+        return search_by_category(value)
 
 
 def analyzer_menu():
@@ -17,14 +35,26 @@ def analyzer_menu():
         ))
 
         options_text = {
-            0: "Digite quantas notícias serão buscadas:",
-            1: "Digite o título:",
-            2: "Digite a data no formato aaaa-mm-dd:",
-            3: "Digite a tag:",
-            4: "Digite a categoria:"
+            "0": "Digite quantas notícias serão buscadas:",
+            "1": "Digite o título:",
+            "2": "Digite a data no formato aaaa-mm-dd:",
+            "3": "Digite a tag:",
+            "4": "Digite a categoria:",
+            "5": top_5_news(),
+            "6": top_5_categories(),
+            "7": sys.stdout.write("Encerrando script\n")
         }
 
-        input(options_text[option])
+        option_str = str(option)
+
+        value = input(options_text[option_str])
+
+        result = function(option_str, value)
+
+        return result
+
+    except KeyError:
+        sys.stderr.write("Opção inválida\n")
 
     except ValueError:
         sys.stderr.write("Opção inválida")
